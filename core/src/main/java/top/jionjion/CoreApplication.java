e05a43aef152af37342b83010a94721d72c46df1
JionJion <1434501783@qq.com>
2020-06-08 00:01:30 +0800

    初始化

---
+++ core/src/main/java/top/jionjion/CoreApplication.java
@@ -0,0 +1,16 @@
+package top.jionjion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jion
+ *  启动类
+ */
+@SpringBootApplication
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+}
