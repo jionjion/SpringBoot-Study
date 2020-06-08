32f77d71b0ebbf8eaf91dac90a4f8a7f2896a9d3
JionJion <1434501783@qq.com>
2020-06-08 20:06:47 +0800

    -

---
+++ core/src/test/java/top/jionjion/CoreApplicationTests.java
@@ -0,0 +1,14 @@
+package top.jionjion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("启动成功");
+    }
+
+}
--- web/src/main/java/top/jionjion/WebApplication.java
+++ web/src/main/java/top/jionjion/WebApplication.java
@@ -3,15 +3,11 @@ package top.jionjion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author Jion
- *  启动类
- */
 @SpringBootApplication
-public class CoreApplication {
+public class WebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CoreApplication.class, args);
+        SpringApplication.run(WebApplication.class, args);
     }
 
 }
