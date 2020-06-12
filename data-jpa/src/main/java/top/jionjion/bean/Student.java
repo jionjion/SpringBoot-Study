29fe8e48e80cda4292df5510f86e98b189c20fb9
JionJion <1434501783@qq.com>
2020-06-12 17:35:27 +0800

    -

--- data-jpa/src/main/java/top/jionjion/bean/Student.java
+++ data-jpa/src/main/java/top/jionjion/bean/Student.java
@@ -1,10 +1,15 @@
 package top.jionjion.bean;
 
+import lombok.Data;
+
 import java.util.Date;
 
 /**
  *	学生类的数据库对象模型
+ *
+ * @author JionJion
  */
+@Data
 public class Student {
 
 	private int id;
@@ -16,51 +21,4 @@ public class Student {
 	private String address;
 	
 	private Date birthday;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public Date getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
-
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", age=" + age + ", address=" + address + ", birthday="
-				+ birthday + "]";
-	}
-
 }
--- data-jpa/src/main/java/top/jionjion/bean/Teacher.java
+++ data-jpa/src/main/java/top/jionjion/bean/Teacher.java
@@ -1,5 +1,9 @@
 package top.jionjion.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -12,8 +16,13 @@ import javax.persistence.Id;
  *	注意使用包装类型替换基本类型
  *	数据库表在创建时随项目启动而创建
  *	JPA定义规范,Hibernate各种实现,ORM框架映射
+ *
+ * 	@author JionJion
  */
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Teacher {
 
 	@Id
@@ -29,68 +38,4 @@ public class Teacher {
 	private String address;
 	
 	private Integer age;
-
-	
-	
-	public Teacher() {
-		super();
-	}
-
-	public Teacher(Integer id, String name, Date workday, String address, Integer age) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.workday = workday;
-		this.address = address;
-		this.age = age;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Date getWorkday() {
-		return workday;
-	}
-
-	public void setWorkday(Date workday) {
-		this.workday = workday;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	@Override
-	public String toString() {
-		return "Teacher [id=" + id + ", name=" + name + ", workday=" + workday + ", address=" + address + ", age=" + age
-				+ "]";
-	}
-	
-	
-
 }
