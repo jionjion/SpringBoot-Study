413597d84b9ce59140ff37cd5e5e567a6a45a762
JionJion <1434501783@qq.com>
2020-06-13 09:44:36 +0800

    -

---
+++ mybatis/src/test/java/top/jionjion/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package top.jionjion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
