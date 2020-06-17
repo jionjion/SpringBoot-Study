d96b67fa1ddc974ad965f438f0c0a11bcc93b4d5
JionJion <1434501783@qq.com>
2020-06-17 22:51:14 +0800

    2020-06-17

--- mybatis/src/main/java/top/jionjion/mapper/StudentMapper.java
+++ mybatis/src/main/java/top/jionjion/mapper/StudentMapper.java
@@ -3,6 +3,10 @@ package top.jionjion.mapper;
 import org.apache.ibatis.annotations.Param;
 import top.jionjion.bean.Student;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Jion
  */
@@ -11,7 +15,97 @@ public interface StudentMapper {
     /**
      *  通过ID查询
      * @param id 主键
-     * @return 查询结果
+     * @return 结果
      */
-    Student findByUserId(@Param("id") Integer id);
+    Student findStudentById(@Param("id") Integer id);
+
+    /**
+     *  通过Name查询,字符
+     * @param name 姓名
+     * @return 结果
+     */
+    List<Student> findStudentByName(String name);
+
+    /**
+     *  通过Name查询, 模糊查询
+     * @param name 姓名
+     * @return 结果
+     */
+    List<Student> findStudentByLikeName(String name);
+
+    /**
+     *  通过Birthday查询, 日期查询
+     * @param birthday 生日, 日期
+     * @return 结果
+     */
+    List<Student> findStudentByBirthdayDate(LocalDate birthday);
+
+    /**
+     *  通过Birthday查询 字符转日期查询
+     * @param birthday 生日,字符串
+     * @return 结果
+     */
+    List<Student> findStudentByBirthdayStr(String birthday);
+
+    /**
+     *  通过Birthday范围查询, 字符范围
+     * @param birthdayFrom 日期字符从
+     * @param birthdayTo  日期字符到
+     * @return 结果
+     */
+    List<Student> findStudentByBirthdayBetween(String birthdayFrom, String birthdayTo);
+
+    /**
+     *  通过ID范围查询, 数字范围
+     * @param idFrom 主键从
+     * @param idTo 主键到
+     * @return 结果
+     */
+    List<Student> findStudentByIdBetween(int idFrom, int idTo);
+
+    /**
+     *  查询全部数据总数
+     * @return 结果
+     */
+    Long findCount();
+
+    /**
+     *  查询包含有伪列的数据
+     * @return 结果
+     */
+    List<Map> findRow();
+
+    /**
+     *  通过ID范围in查询, 数字范围
+     * @param ids 主键范围
+     * @return 结果
+     */
+    List<Student> findStudentByIdIn(List<Integer> ids);
+
+    /**
+     *  通过ID查询大小于, 数字范围
+     * @param id 主键
+     * @return 结果
+     */
+    List<Student> findStudentByIdLt(Integer id);
+
+    /**
+     *  判空查询
+     * @return 结果
+     */
+    List<Student> findStudentByAgeNull();
+
+    /**
+     *  查询排序
+     * @return 结果
+     */
+    List<Student> findAllOrderByIdDesc();
+
+    /**
+     *  分页查询
+     * @param index 页首纪录
+     * @param pageSize 分页大小
+     * @return 结果
+     */
+    List<Student> findAllLimit(int index,  int pageSize);
 }
--- mybatis/src/test/java/top/jionjion/mapper/StudentMapperTest.java
+++ mybatis/src/test/java/top/jionjion/mapper/StudentMapperTest.java
@@ -7,6 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 import top.jionjion.bean.Student;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Jion
  */
@@ -18,9 +23,100 @@ class StudentMapperTest {
     StudentMapper studentMapper;
 
     @Test
-    void findByUserId(){
-        Student student = studentMapper.findByUserId(1);
-        Assert.notNull(student,"查询失败!");
+    void findStudentById(){
+        Student student = studentMapper.findStudentById(1);
+        Assert.notNull(student,"失败!");
         log.info("查询成功" + student.toString());
+    }
+
+    @Test
+    void findStudentByName(){
+        List<Student> studentList = studentMapper.findStudentByName("Jion");
+        Assert.notNull(studentList, "失败!");
+        log.info("查询成功" + studentList.toString());
+    }
+
+    @Test
+    void findByLikeStudentName(){
+        List<Student> studentList = studentMapper.findStudentByLikeName("J");
+        Assert.notNull(studentList, "失败!");
+        log.info("查询成功" + studentList.toString());
+    }
+
+    @Test
+    void findStudentByBirthdayDate(){
+        List<Student> studentList = studentMapper.findStudentByBirthdayDate(LocalDate.of(1994, 4, 12));
+        Assert.notNull(studentList, "失败!");
+        log.info("查询成功" + studentList.toString());
+    }
+
+    @Test
+    void findStudentByBirthdayStr(){
+        List<Student> studentList = studentMapper.findStudentByBirthdayStr("1994-04-12");
+        Assert.notNull(studentList, "失败!");
+        log.info("查询成功" + studentList.toString());
+    }
+
+    @Test
+    void findStudentByBirthdayBetween(){
+        List<Student> studentList = studentMapper.findStudentByBirthdayBetween("1990-01-01", "2000-01-01");
+        Assert.notNull(studentList, "失败!");
+        log.info("查询成功" + studentList.toString());
+    }
+
+    @Test
+    void findStudentByIdBetween(){
+        List<Student> studentList = studentMapper.findStudentByIdBetween(1, 10);
+        Assert.notNull(studentList, "失败!");
+        log.info("查询成功" + studentList.toString());
+    }
+
+    @Test
+    void findCount(){
+        Long count = studentMapper.findCount();
+        Assert.notNull(count, "失败!");
+        log.info("查询成功" + count.toString());
+    }
+
+    @Test
+    void findRow(){
+        List<Map> map = studentMapper.findRow();
+        Assert.notNull(map, "失败!");
+        log.info("查询成功" + map.toString());
+    }
+
+    @Test
+    void findStudentByIdIn(){
+        List<Student> studentList = studentMapper.findStudentByIdIn(Arrays.asList(1, 2, 3));
+        Assert.notNull(studentList, "失败!");
+        log.info("查询成功" + studentList.toString());
+    }
+
+    @Test
+    void findStudentByIdLt(){
+        List<Student> studentList = studentMapper.findStudentByIdLt(5);
+        Assert.notNull(studentList, "失败!");
+        log.info("查询成功" + studentList.toString());
+    }
+
+    @Test
+    void findStudentByAgeNull(){
+        List<Student> studentList = studentMapper.findStudentByAgeNull();
+        Assert.notNull(studentList, "失败!");
+        log.info("查询成功" + studentList.toString());
+    }
+
+    @Test
+    void findAllOrderByIdDesc(){
+        List<Student> studentList = studentMapper.findAllOrderByIdDesc();
+        Assert.notNull(studentList, "失败!");
+        log.info("查询成功" + studentList.toString());
+    }
+
+    @Test
+    void findAllLimit(){
+        List<Student> studentList = studentMapper.findAllLimit(0, 5);
+        Assert.notNull(studentList, "失败!");
+        log.info("查询成功" + studentList.toString());
     }
 }
