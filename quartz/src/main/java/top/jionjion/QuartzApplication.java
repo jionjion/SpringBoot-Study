e5331569c6e4e13942f384df3c99a728683a28dc
JionJion <1434501783@qq.com>
2020-08-12 19:38:30 +0800

    2020-08-12

--- quartz/src/main/java/top/jionjion/QuartzApplication.java
+++ quartz/src/main/java/top/jionjion/QuartzApplication.java
@@ -3,6 +3,11 @@ package top.jionjion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *  定时调度任务
+ *
+ * @author Jion
+ */
 @SpringBootApplication
 public class QuartzApplication {
 
