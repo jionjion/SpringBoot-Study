4f67588a90a35d80ab76339d4bf313219729e033
JionJion <1434501783@qq.com>
2020-11-07 18:21:30 +0800

    2020-11-07

---
+++ rabbit-mq/src/test/java/top/jionjion/rabbit/RabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package top.jionjion.rabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
