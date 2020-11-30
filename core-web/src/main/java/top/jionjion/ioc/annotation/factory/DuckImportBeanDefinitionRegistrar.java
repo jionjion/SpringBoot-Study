2b60fe4b733cba88f6c867e5058fa443da4c95a6
JionJion <1434501783@qq.com>
2020-11-30 22:35:04 +0800

    2020-11-30

--- core-web/src/main/java/top/jionjion/ioc/annotation/factory/DuckImportBeanDefinitionRegistrar.java
+++ core-web/src/main/java/top/jionjion/ioc/annotation/factory/DuckImportBeanDefinitionRegistrar.java
@@ -7,13 +7,17 @@ import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.stereotype.Component;
 
 /**
- *  使用 ImportBeanDefinitionRegistrar 注入Bean
+ *  使用 ImportBeanDefinitionRegistrar 注入Bean.
+ *  常用配置 @Import注解, 在自定义注解中,将当注解注入.同时携带自定义注解中的元信息, 用来向容器中动态添加Bean
+ *
  * @author Jion
  */
 @Component
 public class DuckImportBeanDefinitionRegistrar implements ImportBeanDefinitionRegistrar {
     @Override
     public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+        // importingClassMetadata 在 @Import 注解上的类的注解信息.
+
         // 注入Bean
         RootBeanDefinition rootBeanDefinition = new RootBeanDefinition();
         rootBeanDefinition.setBeanClass(Duck.class);
