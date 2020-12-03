d087d6d8a0a7ad8a2698180102850d70251ed271
JionJion <1434501783@qq.com>
2020-12-03 19:55:20 +0800

    2020-12-03

--- core-web/src/main/java/top/jionjion/servlet/MyWebApplicationInitializer.java
+++ core-web/src/main/java/top/jionjion/servlet/MyWebApplicationInitializer.java
@@ -6,8 +6,15 @@ import org.springframework.web.WebApplicationInitializer;
 import javax.servlet.ServletContext;
 
 /**
- *  虽然实现了 WebApplicationInitializer, 但是不会被调用.
- *  仍然以Tomcat的为主
+ * 上古版本, 在外置 tomcat 启动时, 为 Web容器添加什么....比如过滤器,拦截器什么.
+ * servlet3.0规范中,约定,在 META-INF/services 目录下文件 javax.servlet.ServletContainerInitializer 中写入其同名接口具体实现.
+ * 以便在Web容器启动时,通过java配置容器对象,实现注入组件.
+ * Spring 中默认使用 org.springframework.web.SpringServletContainerInitializer 类来约定容器启动时的加载动作.
+ * 在改实现类中, 具体是通过调用 WebApplicationInitializer 接口的具体实现类完成调用.
+ *
+ * 不过现在都是使用内置Tomcat, 则调用
+ * org.springframework.boot.web.embedded.tomcat.TomcatStarter#onStartup具体实现.
+ * 从而注入 ServletContextInitializer 接口的具体实现类
  *
  * @author Jion
  */
