55f0eea22ad6285dec23fa049c4c06aedc0b20f7
jionjion <jionjion@aliyun.com>
2020-12-29 17:44:57 +0800

    2020-12-29

--- validation/src/main/java/top/jionjion/validation/ValidationApplication.java
+++ validation/src/main/java/top/jionjion/validation/ValidationApplication.java
@@ -3,11 +3,17 @@ package top.jionjion.validation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringBoot验证参数
+ * https://www.cnblogs.com/longronglang/p/12913681.html
+ * https://blog.csdn.net/qq_31142553/article/details/89430100
+ *
+ * @author JionJion
+ */
 @SpringBootApplication
 public class ValidationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ValidationApplication.class, args);
     }
-
 }
