160ffcb1e193218909a031e7945df1c5532760fa
jionjion <jionjion@aliyun.com>
2021-01-04 20:42:25 +0800

    2021-01-04

--- validation/src/test/java/top/jionjion/bean/StudentTest.java
+++ validation/src/test/java/top/jionjion/bean/StudentTest.java
@@ -1,11 +1,15 @@
 package top.jionjion.bean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Jion
  */
 class StudentTest {
 
-
+    @Test
+    public void test(){
+        Student student = new Student();
+        System.out.println(student);
+    }
 }
