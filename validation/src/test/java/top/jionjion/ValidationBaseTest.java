4e4932775a3c6098803c3785b6f1de19b65c5cb3
jionjion <jionjion@aliyun.com>
2021-01-13 16:23:57 +0800

    2021-01-13

--- validation/src/test/java/top/jionjion/ValidationBaseTest.java
+++ validation/src/test/java/top/jionjion/ValidationBaseTest.java
@@ -3,7 +3,6 @@ package top.jionjion;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.ConstraintViolation;
-import javax.validation.Valid;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import java.util.Set;
@@ -21,7 +20,7 @@ public abstract class ValidationBaseTest {
      *
      * @param obj 验证对象
      */
-    public void validate(@Valid Object obj) {
+    public void validate(Object obj) {
         // 验证器
         Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
@@ -40,9 +39,58 @@ public abstract class ValidationBaseTest {
     }
 
     /**
-     *  分组验证对象是否符合要求
+     * 验证对象属性是否符合要求
      *
-     * @param obj 验证对象
+     * @param obj          验证对象
+     * @param propertyName 属性名称
+     */
+    public void validateProperty(Object obj, String propertyName) {
+
+        // 验证器
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+        // 验证错误
+        Set<ConstraintViolation<Object>> validateSet = validator.validateProperty(obj, propertyName);
+
+        log.info("=========================================");
+        // 结果返回
+        for (ConstraintViolation<Object> violation : validateSet) {
+            // key:对象属性路径,  value:校验消息
+            // 校验结果
+            log.info(violation.toString());
+        }
+        log.info("=========================================\n");
+    }
+
+    /**
+     * 验证对象属性值是否符合要求
+     *
+     * @param clazz        验证对象的类
+     * @param propertyName 属性名称
+     * @param value        尝试的值
+     */
+    public void validatePropertyValues(Class<?> clazz, String propertyName, Object value) {
+
+        // 验证器
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+        // 验证错误
+        Set<? extends ConstraintViolation<?>> validateSet = validator.validateValue(clazz, propertyName, value);
+
+        log.info("=========================================");
+        // 结果返回
+        for (ConstraintViolation<?> violation : validateSet) {
+            // key:对象属性路径,  value:校验消息
+            // 校验结果
+            log.info(violation.toString());
+        }
+        log.info("=========================================\n");
+    }
+
+    /**
+     * 分组验证对象是否符合要求
+     *
+     * @param obj    验证对象
      * @param groups 分组依据
      */
     public void validate(Object obj, Class<?>... groups) {
--- validation/src/test/java/top/jionjion/bean/StudentTest.java
+++ validation/src/test/java/top/jionjion/bean/StudentTest.java
@@ -1,15 +1,30 @@
 package top.jionjion.bean;
 
 import org.junit.jupiter.api.Test;
+import top.jionjion.ValidationBaseTest;
 
 /**
  * @author Jion
  */
-class StudentTest {
+class StudentTest extends ValidationBaseTest {
 
     @Test
-    public void test(){
+    public void validate(){
         Student student = new Student();
-        System.out.println(student);
+        super.validate(student);
+    }
+
+    @Test
+    public void validateProperty(){
+        Student student = new Student();
+        super.validateProperty(student, "name");
+    }
+
+    @Test
+    public void validatePropertyValues(){
+        Student student = new Student();
+        student.setName("Jion");
+        super.validatePropertyValues(Student.class, "name", "Jion");
+        super.validatePropertyValues(Student.class, "name", null);
     }
 }
