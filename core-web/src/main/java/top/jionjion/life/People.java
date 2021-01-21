c9061c32478b981b6b5814b52dac115eee94aa5b
jionjion <jionjion@aliyun.com>
2021-01-21 19:27:17 +0800

    2021-01-21

--- core-web/src/main/java/top/jionjion/life/People.java
+++ core-web/src/main/java/top/jionjion/life/People.java
@@ -4,12 +4,13 @@ import lombok.Data;
 import org.springframework.stereotype.Component;
 
 /**
- *  Bean的声明周期
+ * Bean的生命周期
+ *
  * @author Jion
  */
 @Data
 @Component
 public class People {
 
-    private  String name;
+    private String name;
 }
