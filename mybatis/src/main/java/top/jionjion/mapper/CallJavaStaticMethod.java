244c3d30b3ee705320072e27a7a0553439dce7e0
jionjion <jionjion@aliyun.com>
2021-01-29 14:35:47 +0800

    2021-01-29

---
+++ mybatis/src/main/java/top/jionjion/mapper/CallJavaStaticMethod.java
@@ -0,0 +1,15 @@
+package top.jionjion.mapper;
+
+/**
+ * 调用java的方法
+ *
+ * @author Jion
+ */
+public interface CallJavaStaticMethod {
+
+    /**
+     *  调用Java的静态方法
+     * @param name 调用参数
+     */
+    void findStudentByName(String name);
+}
---
+++ mybatis/src/main/java/top/jionjion/util/MybatisStringUtils.java
@@ -0,0 +1,30 @@
+package top.jionjion.util;
+
+import java.util.Locale;
+
+/**
+ * 字符处理,在 Mybatis 中调用必须为静态方法
+ *
+ * @author Jion
+ */
+public class MybatisStringUtils {
+
+    /**
+     * 小写转大写
+     *
+     * @param lower 输入字符
+     * @return 转为大写
+     */
+    public static String toUpperCase(String lower) {
+        return lower.toUpperCase(Locale.ROOT);
+    }
+
+    /**
+     * 返回 A
+     *
+     * @return A
+     */
+    public static String returnA() {
+        return "A";
+    }
+}
