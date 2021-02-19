758424bb164c85ca274a6b9d636606c122ddf810
jionjion <jionjion@aliyun.com>
2021-02-19 10:58:32 +0800

    2021-02-19

---
+++ mybatis/src/test/plus/top/jionjion/service/impl/TeacherServiceImplTest.java
@@ -0,0 +1,27 @@
+package top.jionjion.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import top.jionjion.dto.Teacher;
+import top.jionjion.service.ITeacherService;
+
+/**
+ * 测试封装的Service类的接口
+ *
+ * @author Jion
+ */
+@Slf4j
+@SpringBootTest
+class TeacherServiceImplTest {
+
+    @Autowired
+    ITeacherService teacherService;
+
+    @Test
+    public void test() {
+        Teacher teacher = teacherService.getById(1);
+        log.info("执行查询, {}", teacher);
+    }
+}
