60852b5219156cecdf56ba70aae7a6a83be73313
jionjion <jionjion@aliyun.com>
2022-02-15 15:41:21 +0800

    2022-02-15

---
+++ knife4j/src/main/java/top/jionjion/knife4j/Knife4jApplication.java
@@ -0,0 +1,21 @@
+package top.jionjion.knife4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+/**
+ * 使用 Knife4j 进行文档管理
+ * 文档地址 http://localhost:8080/doc.html
+ *
+ * @author Jion
+ */
+@EnableOpenApi
+@SpringBootApplication
+public class Knife4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Knife4jApplication.class, args);
+    }
+
+}
