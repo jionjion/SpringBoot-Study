f768316d5f297d3720f95ee03581ee347b2fa4f8
jionjion <jionjion@aliyun.com>
2022-02-24 20:32:03 +0800

    2022-02-24

---
+++ quartz/src/main/java/top/jionjion/quartz/jobs/SendEmailJob.java
@@ -0,0 +1,27 @@
+package top.jionjion.quartz.jobs;
+
+
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+/**
+ * 模拟定时发送邮件任务.
+ * Job类, 定义具体的业务
+ *
+ * @author Jion
+ */
+public class SendEmailJob extends QuartzJobBean {
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) {
+        // 从上下文中获得执行参数
+        JobDataMap jobDataMap = context.getMergedJobDataMap();
+
+        if (jobDataMap != null && !jobDataMap.isEmpty()) {
+            System.out.println("******************************");
+            System.out.println("job name = " + jobDataMap.get("TASK_NAME"));
+            System.out.println("开始发送邮件了");
+        }
+    }
+}
--- quartz/src/test/java/top/jionjion/quartz/QuartzApplicationTests.java
+++ quartz/src/test/java/top/jionjion/quartz/QuartzApplicationTests.java
@@ -1,4 +1,4 @@
-package top.jionjion;
+package top.jionjion.quartz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
