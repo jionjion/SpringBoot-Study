8c1b582ff4f28546a8ebc34a900a7c18dafe226e
jionjion <jionjion@aliyun.com>
2022-02-28 18:21:36 +0800

    2022-02-28

---
+++ elasticseach/src/test/java/top/jionjion/elasticsearch/ElasticsearchApplicationTests.java
@@ -0,0 +1,14 @@
+package top.jionjion.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
