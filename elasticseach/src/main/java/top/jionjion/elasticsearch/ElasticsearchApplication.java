b0575ba971a3c3b918ff0ec51ab430604cfe74f8
jionjion <jionjion@aliyun.com>
2022-03-02 23:52:12 +0800

    2022-03-02

--- elasticseach/src/main/java/top/jionjion/elasticsearch/ElasticsearchApplication.java
+++ elasticseach/src/main/java/top/jionjion/elasticsearch/ElasticsearchApplication.java
@@ -3,11 +3,15 @@ package top.jionjion.elasticsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 参考 https://blog.csdn.net/promsing/article/details/122876032?
+ *
+ * @author JionJion
+ */
 @SpringBootApplication
 public class ElasticsearchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ElasticsearchApplication.class, args);
     }
-
 }
