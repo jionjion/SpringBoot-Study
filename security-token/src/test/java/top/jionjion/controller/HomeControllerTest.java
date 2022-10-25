7cd7bf7cf47384ef81e5a23d686ed75339f92333
jionjion <jionjion@aliyun.com>
2022-10-25 09:29:27 +0800

    2022-10-25

--- security-token/src/test/java/top/jionjion/controller/HomeControllerTest.java
+++ security-token/src/test/java/top/jionjion/controller/HomeControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -15,28 +16,28 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Collections;
 
 /**
  * @author Jion
  */
-@SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HomeControllerTest {
 
     @Autowired
-    private HomeController homeController;
+    WebApplicationContext webApplicationContext;
 
     MockMvc mockMvc;
 
-    String token;
-
     final Logger logger = LoggerFactory.getLogger(HomeControllerTest.class);
+
 
     @BeforeEach
     void init() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(SecurityMockMvcConfigurers.springSecurity()).build();
     }
 
     /**
@@ -60,6 +61,11 @@ class HomeControllerTest {
     @Test
     @Order(200)
     void login() throws Exception {
+        String token = doLogin();
+        Assertions.assertNotNull(token, "未获取 Token.");
+    }
+
+    private String doLogin() throws Exception {
         LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.put("username", Collections.singletonList("admin"));
         params.put("password", Collections.singletonList("123456"));
@@ -72,7 +78,7 @@ class HomeControllerTest {
         MockHttpServletResponse response = mvcResult.getResponse();
         String token = response.getContentAsString();
         logger.info("获得 Token:  {}", token);
-        this.token = token;
+        return token;
     }
 
     /**
@@ -83,11 +89,11 @@ class HomeControllerTest {
     @Test
     @Order(300)
     void authorityInfo() throws Exception {
+        String token = doLogin();
         // 构建请求
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/authority")
-                .header("Authorization", Collections.singletonList(String.format("Bearer %s", this.token)))
+                .header("Authorization", Collections.singletonList(String.format("Bearer %s", token)))
                 .accept(MediaType.APPLICATION_JSON);
-
         // 请求
         MvcResult mvcResult = this.mockMvc.perform(requestBuilder)
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -106,10 +112,11 @@ class HomeControllerTest {
     @Test
     @Order(400)
     void roleInfo() throws Exception {
+        String token = doLogin();
         this.mockMvc.perform(MockMvcRequestBuilders.get("/role")
-                        .header("Authorization", Collections.singletonList(String.format("Bearer %s", this.token)))
+                        .header("Authorization", Collections.singletonList(String.format("Bearer %s", token)))
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
                 .andDo(MockMvcResultHandlers.print());
     }
 }
